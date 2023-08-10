058a2f461295bb40dc6a0970f1436c14ea63a01d
Austin Ohlfs <ohlfsam96@gmail.com>
2023-08-10 11:20:41 -0600

    complete lab

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a, b) {
+return a + b;
+} 
+
+function subtract(a, b) {
+return a - b;
+}
+
+function multiply(a, b) {
+return a * b;
+}
+
+function divide(a, b) {
+return a / b;
+}
+
+
+
+let number = 5;
+function increment(number) {
+number++;
+return number;
+
+}
+
+let number2 = 5;
+function decrement(number2) {
+number2--;
+return number2;
+
+}
+
+function makeInt(string) {
+return parseInt(string, 10);
+}
+
+function preserveDecimal(string) {
+  return parseFloat(string);
+}
